75ba3d6b068704a42c11f40d57c32d641d7d101b
Gabriel <gabriel.developer@outlook.com>
2020-02-09 23:18:34 -0300

    iniciando servidor

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const http = require('http');
 
-const servidor = http.createServer();
+const servidor = http.createServer(function(req, resp){
+    resp.end(`<h1>Servidor iniciado</h1>`);
+});
 
 servidor.listen(3000);
