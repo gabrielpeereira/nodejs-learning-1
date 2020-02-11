9176224d8fcb120d3067b08f5c82d97059be03be
Gabriel <gabriel.developer@outlook.com>
2020-02-11 00:12:02 -0300

    encapsulando modulos nodejs

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-const express = require('express');
-
-const app = express();
+const app = require('./src/config/custom-express');
 
 app.listen(3000, function(){
     console.log(`Servidor iniciado na porta 3000`);
