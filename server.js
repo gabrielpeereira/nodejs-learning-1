bafd36e1f004214cc6d00f748a1c024efe4bbaf1
Gabriel <gabriel.developer@outlook.com>
2020-02-12 00:31:50 -0300

    encapsulando rotas

--- server.js
+++ server.js
@@ -4,17 +4,3 @@ app.listen(3000, function(){
     console.log(`Servidor iniciado na porta 3000`);
 });
 
-app.get('/', function(req, resp){
-   resp.send(
-    `<!DOCTYPE html>
-    <html lang="pt-br">
-        <head>
-            <meta charset="utf-8">
-        </head>
-        <body>
-            <h1>Servidor iniciado</h1>
-        </body>
-    </html>
- `
-   ) 
-});
---
+++ src/app/rotas/rotas.js
@@ -0,0 +1,32 @@
+module.exports = (app) => {
+    app.get('/', function(req, resp){
+        resp.send(
+         `<!DOCTYPE html>
+         <html lang="pt-br">
+             <head>
+                 <meta charset="utf-8">
+             </head>
+             <body>
+                 <h1>Servidor iniciado</h1>
+             </body>
+         </html>
+      `
+        ) 
+     });
+
+     app.get('/livros', function(req, resp){
+        resp.send(
+         `<!DOCTYPE html>
+         <html lang="pt-br">
+             <head>
+                 <meta charset="utf-8">
+             </head>
+             <body>
+                 <h1>Livraria</h1>
+             </body>
+         </html>
+      `
+        ) 
+     });
+};
+
